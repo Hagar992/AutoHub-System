396bc0fd1d3f3be054fd17bb9381952ba7dbf5be
YARYA412001 <shadowyarya@gmail.com>
2025-11-17 14:37:40 +0200

    Save my changes

--- AutoHub-System/Migrations/20251117120725_IntialCreate.Designer.cs
+++ AutoHub-System/Migrations/20251117120725_IntialCreate.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AutoHub_System.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20251117081513_InitialCreate")]
-    partial class InitialCreate
+    [Migration("20251117120725_IntialCreate")]
+    partial class IntialCreate
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
